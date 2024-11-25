eb8a6a2e2f71baec1e5cac39f8eae9bc077c431e
tospery <tospery@gmail.com>
2024-11-25 21:05:51 +0800

    Examples

---
+++ Example/SwiftUIKit-Hi_Example/SwiftUIKit_Hi_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKit_Hi_ExampleApp.swift
+//  SwiftUIKit-Hi_Example
+//
+//  Created by 杨建祥 on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKit_Hi_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
